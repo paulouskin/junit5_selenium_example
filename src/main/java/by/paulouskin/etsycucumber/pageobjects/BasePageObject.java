db0963ccc87ea669ca5c5199cd3f5840a61b1027
paulouskin <pavlovskin88@gmail.com>
2018-08-06 10:06:19 +0200

    Added screenshot capturing

--- src/main/java/by/paulouskin/etsycucumber/pageobjects/BasePageObject.java
+++ src/main/java/by/paulouskin/etsycucumber/pageobjects/BasePageObject.java
@@ -1,11 +1,14 @@
 package by.paulouskin.etsycucumber.pageobjects;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BasePageObject {
@@ -28,4 +31,15 @@ public class BasePageObject {
                 until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
     }
 
+    public void captureScreenShot() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
+        String time = LocalDateTime.now().format(dtf);
+        File screenshot1 = new File("target/screenshot"+time+".png");
+        File outFile = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+        try {
+            Files.copy(outFile.toPath(), screenshot1.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
