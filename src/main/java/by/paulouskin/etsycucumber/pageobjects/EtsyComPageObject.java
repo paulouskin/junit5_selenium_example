c3ae10bedb216c7b38fc4c21972e3c13c4bfe182
paulouskin <pavlovskin88@gmail.com>
2018-10-30 09:58:09 +0100

    button selector update

--- src/main/java/by/paulouskin/etsycucumber/pageobjects/EtsyComPageObject.java
+++ src/main/java/by/paulouskin/etsycucumber/pageobjects/EtsyComPageObject.java
@@ -15,6 +15,7 @@ public class EtsyComPageObject extends BasePageObject{
     private final String SHOP_LOCATION_SEARCH_BUTTON = "button.shop-location-submit";
     private final String SHOP_LOCATION_TEXT_FIELD = "#shop-location-input";
     private final String TERMS_AND_CONDITIONS_WRAPPER = "div.alert";
+    private final String ACCEPT_POLICY_BUTTON = "button.btn-outline-black";
     private final String SEARCH_FIELD = "#search-query";
     private  String FILTER_FOR_CATEGORY_LINK = "//h5[text()='%s']/../..//a[contains(.,'%s')]"; //Check
 
@@ -25,7 +26,7 @@ public class EtsyComPageObject extends BasePageObject{
 
     public void acceptTermsAndConditions() {
         findElementWithWait(By.cssSelector(TERMS_AND_CONDITIONS_WRAPPER));
-        findElementWithWait(By.cssSelector("button.btn-outline-black")).click();
+        findElementWithWait(By.cssSelector(ACCEPT_POLICY_BUTTON)).click();
     }
 
     public void searchForItem(String query) {
